809dc1a7431f41537c7d5d4471c6172739844d06
zhaozhanbin <354816915@qq.com>
2018-12-06 19:39:55 +0800

    创建分支

--- ocean-fresh-feign1/src/main/java/com/jk/service/SchedualServiceHi.java
+++ ocean-fresh-feign1/src/main/java/com/jk/service/SchedualServiceHi.java
@@ -1,7 +1,8 @@
 package com.jk.service;
 
 import model.User;
-import org.springframework.cloud.netflix.feign.FeignClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
